10481768b110d4050fd6180010b2f0c036488919
Dimitar Dimitrov <62475479+DimitarDimitrov369@users.noreply.github.com>
2023-06-17 14:45:14 +0300

    Add project files.

---
+++ 01.TempleОfDoom/01.TempleОfDoom/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System;
+
+Queue<int> tools = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Stack<int> substances = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+List<int> challenges = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+while (challenges.Any())
+{
+    int tool = tools.Dequeue();
+    int substance = substances.Pop();
+    int result = tool * substance;
+
+    if (challenges.Contains(result))
+    {
+        challenges.Remove(result);
+    }
+
+    else
+    {
+        tools.Enqueue(tool + 1);
+        int newSubstance = substance - 1;
+        if (newSubstance != 0)
+        {
+            substances.Push(newSubstance);
+        }
+    }
+
+    if (!tools.Any() || !substances.Any())
+    {
+        if (challenges.Any())
+        {
+            Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+            if (tools.Any())
+            {
+                Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+            }
+            if (substances.Any())
+            {
+                Console.WriteLine($"Substances: {string.Join(", ", substances)}");
+            }
+            if (challenges.Any())
+            {
+                Console.WriteLine($"Challenges: {string.Join(", ", challenges)}");
+            }
+            return;
+        }
+    }
+}
+
+Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");
+if (tools.Any())
+{
+    Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+}
+if (substances.Any())
+{
+    Console.WriteLine($"Substances: {string.Join(", ", substances)}");
+}
---
+++ 02.MouseInTheKitchen/MouseInTheKitchen/Program.cs
@@ -0,0 +1,216 @@
+﻿int[] dimensions = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+int rows = dimensions[0];
+int cols = dimensions[1];
+char[,] matrix = new char[rows, cols];
+int totalCheese = 0;
+int eatenCheese = 0;
+int mouseRow = 0;
+int mouseCol = 0;
+for (int row = 0; row < rows; row++)
+{
+    string rowData = Console.ReadLine();
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = rowData[col];
+        if (matrix[row, col] == 'M')
+        {
+            mouseRow = row;
+            mouseCol = col;
+        }
+
+        if (matrix[row, col] == 'C')
+        {
+            totalCheese++;
+        }
+    }
+}
+
+string command;
+while ((command = Console.ReadLine()) != "danger")
+{
+    switch (command)
+    {
+        case "up":
+            mouseRow--;
+            if (isInsideMatrix(mouseRow, mouseCol, matrix))
+            {
+                if (matrix[mouseRow, mouseCol] == 'C')
+                {
+                    eatenCheese++;
+                    matrix[mouseRow + 1, mouseCol] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                }
+                else if (matrix[mouseRow, mouseCol] == '*')
+                {
+                    matrix[mouseRow + 1, mouseCol] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                }
+
+                else if (matrix[mouseRow, mouseCol] == 'T')
+                {
+                    matrix[mouseRow + 1, mouseCol] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                    Console.WriteLine("Mouse is trapped!");
+                    PrintMatrix(matrix);
+                    return;
+                }
+                else if (matrix[mouseRow, mouseCol] == '@')
+                {
+                    mouseRow++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("No more cheese for tonight!");
+                PrintMatrix(matrix);
+                return;
+            }
+            break;
+
+        case "down":
+            mouseRow++;
+            if (isInsideMatrix(mouseRow, mouseCol, matrix))
+            {
+                if (matrix[mouseRow, mouseCol] == 'C')
+                {
+                    eatenCheese++;
+                    matrix[mouseRow - 1, mouseCol] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+
+                }
+                else if (matrix[mouseRow, mouseCol] == '*')
+                {
+                    matrix[mouseRow - 1, mouseCol] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                }
+
+                else if (matrix[mouseRow, mouseCol] == 'T')
+                {
+                    matrix[mouseRow - 1, mouseCol] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                    Console.WriteLine("Mouse is trapped!");
+                    PrintMatrix(matrix);
+                    return;
+                }
+                else if (matrix[mouseRow, mouseCol] == '@')
+                {
+                    mouseRow--;
+                }
+            }
+            else
+            {
+                Console.WriteLine("No more cheese for tonight!");
+                PrintMatrix(matrix);
+                return;
+            }
+            break;
+
+        case "left":
+            mouseCol--;
+            if (isInsideMatrix(mouseRow, mouseCol, matrix))
+            {
+                if (matrix[mouseRow, mouseCol] == 'C')
+                {
+                    eatenCheese++;
+                    matrix[mouseRow, mouseCol + 1] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                }
+                else if (matrix[mouseRow, mouseCol] == '*')
+                {
+                    matrix[mouseRow, mouseCol + 1] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                }
+                else if (matrix[mouseRow, mouseCol] == 'T')
+                {
+                    matrix[mouseRow, mouseCol + 1] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                    Console.WriteLine("Mouse is trapped!");
+                    PrintMatrix(matrix);
+                    return;
+                }
+                else if (matrix[mouseRow, mouseCol] == '@')
+                {
+                    mouseCol++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("No more cheese for tonight!");
+                PrintMatrix(matrix);
+                return;
+            }
+            break;
+        case "right":
+            mouseCol++;
+            if (isInsideMatrix(mouseRow, mouseCol, matrix))
+            {
+                if (matrix[mouseRow, mouseCol] == 'C')
+                {
+                    eatenCheese++;
+                    matrix[mouseRow, mouseCol - 1] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                }
+                else if (matrix[mouseRow, mouseCol] == '*')
+                {
+                    matrix[mouseRow, mouseCol - 1] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                }
+
+                else if (matrix[mouseRow, mouseCol] == 'T')
+                {
+                    matrix[mouseRow, mouseCol - 1] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                    Console.WriteLine("Mouse is trapped!");
+                    PrintMatrix(matrix);
+                    return;
+                }
+                else if (matrix[mouseRow, mouseCol] == '*')
+                {
+                    matrix[mouseRow, mouseCol - 1] = '*';
+                    matrix[mouseRow, mouseCol] = 'M';
+                }
+                else if (matrix[mouseRow, mouseCol] == '@')
+                {
+                    mouseCol--;
+                }
+            }
+            else
+            {
+                Console.WriteLine("No more cheese for tonight!");
+                PrintMatrix(matrix);
+                return;
+
+            }
+            break;
+    }
+
+    if (eatenCheese == totalCheese)
+    {
+        Console.WriteLine("Happy mouse! All the cheese is eaten, good night!");
+        PrintMatrix(matrix);
+        return;
+    }
+
+}
+
+Console.WriteLine("Mouse will come back later!");
+PrintMatrix(matrix);
+bool isInsideMatrix(int row, int col, char[,] matrix)
+{
+    bool isValidMove = row >= 0 && row < matrix.GetLength(0)
+                                && col >= 0 && col < matrix.GetLength(1);
+
+    return isValidMove;
+}
+void PrintMatrix(char[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            Console.Write($"{matrix[row, col]}");
+        }
+
+        Console.WriteLine();
+    }
+}
